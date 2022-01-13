728080c5b227fb1da1c8f1bd66bdfe276ecab80d
kusalhettiarachchi <kusal.14@cse.mrt.ac.lk>
2022-01-14 02:43:28 +0530

    introduced anti pattern matching

--- wordle.py
+++ wordle.py
@@ -1,9 +1,15 @@
 import random
 from re import compile
-from typing import Counter
+from typing import Counter, List, Pattern, Set
+
 
 common_starting_letters     = ['t', 'a', 'o', 'd', 'w']
 common_ending_letters       = ['t', 's', 'e', 'd']
+
+
+class VocabularyExhaustedException(Exception):
+    pass
+
 
 def build_starting_word_vocab():
     import os
@@ -38,11 +44,11 @@ def pick_one(options) -> str:
         word: suggested word
     """
     if len(options) == 0:
-        raise Exception('ran out of words in vocabulary!')
+        raise VocabularyExhaustedException()
     return options[random.randint(0, len(options) - 1)]
 
 
-def suggest_words(step=0, disqualified_letters=set(), qualified_letters=set(), pattern=None, words=[], good_starting_words=[]) -> str:
+def suggest_words(step=0, disqualified_letters=set(), qualified_letters=set(), pattern=None, disqualified_patterns=[], words=[], good_starting_words=[]) -> str:
     """suggests a word given the updated parameters
 
     Args:
@@ -68,14 +74,15 @@ def suggest_words(step=0, disqualified_letters=set(), qualified_letters=set(), p
     legal_words = [w for w in words \
         if len(set(w).intersection(qualified_letters)) == len(qualified_letters) \
             and set(w).intersection(disqualified_letters)  == set() \
-                # and len(Counter(w).keys()) == 5 \
                     and (pattern.match(w) is not None if pattern else True)\
+                        and sum([anti_pattern.match(w) is None for anti_pattern in disqualified_patterns]) == len(disqualified_patterns)
                         ]
     return pick_one(legal_words)
 
 
-def parse_output(suggestion, output, disqualified_letters, qualified_letters, pattern):
+def parse_output(suggestion: str, output: str, disqualified_letters: Set, qualified_letters: Set, pattern: Pattern, disqualified_patterns: List[Pattern]):
     for i, v in enumerate(output):
+        anti_pattern = [r'\w'] * 5
         if v == '0' and suggestion[i] not in qualified_letters:
             disqualified_letters.add(suggestion[i])
         elif v == '1':
@@ -83,7 +90,10 @@ def parse_output(suggestion, output, disqualified_letters, qualified_letters, pa
             pattern[i] = suggestion[i]
         elif v == '2':
             qualified_letters.add(suggestion[i])
-    return disqualified_letters, qualified_letters, pattern
+            # we have atleast one misplaced letter
+            anti_pattern[i] = suggestion[i]
+            disqualified_patterns.append(compile("".join(anti_pattern)))
+    return disqualified_letters, qualified_letters, pattern, disqualified_patterns
 
 
 def play(seed):
@@ -91,6 +101,7 @@ def play(seed):
     dl      = set()
     ql      = set()
     r       = ['\w'] * 5
+    dr      = list()
     random.seed(seed)
     
     step = 0
@@ -98,13 +109,17 @@ def play(seed):
     while True:
         step += 1
         pattern = compile("".join(r))
-        suggestion  = suggest_words(step, disqualified_letters=dl, qualified_letters=ql, words=w, good_starting_words=sw, pattern=pattern)
+        try:
+            suggestion  = suggest_words(step, disqualified_letters=dl, qualified_letters=ql, words=w, good_starting_words=sw, pattern=pattern, disqualified_patterns=dr)
+        except VocabularyExhaustedException:
+            print(f'vocabulary exhausted\ntry regex: {str(pattern)}\nqualified letters: {ql}\ndisqualified letters: {dl}\ndisqualified patterns: {dr}')
+            break
         game_output = input(f'try the word: {suggestion}\noutput: ')
         if game_output == '11111':
             print(f'WON!\nattempts: {step}')
             break
         else:
-            dl, ql, r = parse_output(suggestion, game_output, dl, ql, r)
+            dl, ql, r, dr = parse_output(suggestion, game_output, dl, ql, r, dr)
 
 """
 `output` consists of 5 digits representing the tiles
@@ -113,4 +128,4 @@ def play(seed):
 2 - yellow
 """
 build_starting_word_vocab()
-play(891)
+play(534)
